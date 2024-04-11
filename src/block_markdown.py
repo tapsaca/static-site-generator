ca58a900be8ecf926565f5425d99ec9295ff1bb8
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-04-11 17:27:03 +0300

    Add function for getting block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,31 @@
+def block_to_block_type(block: str):
+    if block.startswith("# ") or block.startswith("## ") or block.startswith("### ") or block.startswith("#### ") or block.startswith("##### ") or block.startswith("###### "):
+        return "heading"
+    elif block.startswith("```") and block.endswith("```"):
+        return "code"
+    elif block.startswith(">"):
+        for line in block.split("\n"):
+            if not line.startswith(">"):
+                return "paragraph"
+        return "quote"
+    elif block.startswith("* "):
+        for line in block.split("\n"):
+            if not line.startswith("* "):
+                return "paragraph"
+        return "unordered_list"
+    elif block.startswith("- "):
+        for line in block.split("\n"):
+            if not line.startswith("- "):
+                return "paragraph"
+        return "unordered_list"
+    elif block.startswith("1. "):
+        lines = block.split("\n")
+        for i in range(len(lines)):
+            if not lines[i].startswith(f"{i + 1}. "):
+                return "paragraph"
+        return "ordered_list"
+    return "paragraph"
+
 def markdown_to_blocks(markdown: str):
     initial_blocks = markdown.split("\n\n")
     new_blocks = []
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,7 +1,37 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import block_to_block_type, markdown_to_blocks
 
 class TestBlockMarkdown(unittest.TestCase):
+    def test_block_to_block_type(self):
+        blocks = [
+            "# heading",
+            "######## actually a paragraph",
+            "```code\nblock```",
+            "```not a code block\nbut paragraph``",
+            ">quote\n>another quote\n>and another",
+            ">quote\nthis is not",
+            "* list item\n* another list item",
+            "- list item\n- another list item",
+            "* list item\nnot a list item",
+            "- list item\nnot a list item",
+            "1. list item\n2. another list item",
+            "2. list\n3. should start with 1.",
+            "1. list item\n3. this should be 2."
+        ]
+        self.assertEqual(block_to_block_type(blocks[0]), "heading")
+        self.assertEqual(block_to_block_type(blocks[1]), "paragraph")
+        self.assertEqual(block_to_block_type(blocks[2]), "code")
+        self.assertEqual(block_to_block_type(blocks[3]), "paragraph")
+        self.assertEqual(block_to_block_type(blocks[4]), "quote")
+        self.assertEqual(block_to_block_type(blocks[5]), "paragraph")
+        self.assertEqual(block_to_block_type(blocks[6]), "unordered_list")
+        self.assertEqual(block_to_block_type(blocks[7]), "unordered_list")
+        self.assertEqual(block_to_block_type(blocks[8]), "paragraph")
+        self.assertEqual(block_to_block_type(blocks[9]), "paragraph")
+        self.assertEqual(block_to_block_type(blocks[10]), "ordered_list")
+        self.assertEqual(block_to_block_type(blocks[11]), "paragraph")
+        self.assertEqual(block_to_block_type(blocks[12]), "paragraph")
+
     def test_markdown_to_blocks(self):
         markdown = """# This is a heading
 
