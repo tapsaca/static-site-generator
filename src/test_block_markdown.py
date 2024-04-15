f2874e512a7efa0d49c6c7fae7dce583c62ae310
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-04-15 14:23:11 +0300

    Add tests

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,7 @@
 import unittest
-from block_markdown import block_to_block_type, markdown_to_blocks
+from block_markdown import block_to_block_type, markdown_to_blocks, heading_to_html_node, paragraph_to_html_node
+from leafnode import LeafNode
+from parentnode import ParentNode
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_block_to_block_type(self):
@@ -49,6 +51,19 @@ class TestBlockMarkdown(unittest.TestCase):
                 "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
                 "* This is a list item\n* This is another list item"
             ]
+        )
+    
+    def test_heading_to_html_node(self):
+        heading_lvl_1 = heading_to_html_node("# This is a level **1** heading")
+        heading_lvl_6 = heading_to_html_node("###### This is a level 6 heading")
+        self.assertEqual(heading_lvl_1.to_html(), "<h1>This is a level <b>1</b> heading</h1>")
+        self.assertEqual(heading_lvl_6.to_html(), "<h6>This is a level 6 heading</h6>")
+    
+    def test_paragraph_to_html_node(self):
+        html_node = paragraph_to_html_node("This is a paragraph of text. It has some **bold** and *italic* words inside of it.")
+        self.assertEqual(
+            html_node.to_html(),
+            "<p>This is a paragraph of text. It has some <b>bold</b> and <i>italic</i> words inside of it.</p>"
         )
         
 if __name__ == "__main__":
