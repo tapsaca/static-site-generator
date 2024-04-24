f4b9b04eed23a0d4ad355cecb627d581cdb28984
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-04-24 12:05:56 +0300

    Implement the main function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from textnode import TextNode
+import os
+import shutil
+from copystatic import copy_files
+
+dir_path_public = "./public"
+dir_path_static = "./static"
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    copy_files(dir_path_static, dir_path_public)
 
 main()
