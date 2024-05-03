c6d073c9eff0c958ee602e27b7cfb3e99e5bbb55
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-05-03 09:32:09 +0300

    Update the main function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import os
 import shutil
 from copystatic import copy_files
+from gencontent import generate_pages_recursively
 
+dir_path_content = "./content"
 dir_path_public = "./public"
 dir_path_static = "./static"
 
@@ -9,5 +11,6 @@ def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     copy_files(dir_path_static, dir_path_public)
+    generate_pages_recursively(dir_path_content, "./template.html", dir_path_public)
 
 main()
